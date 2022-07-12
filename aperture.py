9a4c55bfe0cda8fe23d3a15ea106caf82cc5d2d6
Robby Wilson <robert.forrest.wilson@gmail.com>
2022-07-12 14:53:26 -0400

    speed up for aperture photometry code

--- aperture.py
+++ aperture.py
@@ -1,53 +1,82 @@
 from photutils.aperture import CircularAperture, aperture_photometry
 from astropy.table import vstack
 from astropy.io import fits
+import fitsio
+import numpy as np
 
 
-
-
-class PhotObject(object):
+class ApPhot(object):
     
     def __init__(self,fnames,x,y,aperture_radii=[1.,1.5,2.,2.5,3.,3.5,4.]):
         
         self.fnames=sorted(fnames)
         self.x=x
         self.y=y
-        self.apertures=self._make_apertures(aperture_radii)
+        self.aperture_radii = aperture_radii
+        self.apertures = None
 
-    def _make_apertures(self, apradii):
+    def _make_apertures(self, apradii, dx):
 
-        x0, y0 = self.x, self.y
-        circapers = [CircularAperture((x0,y0), aprad ) for aprad in apradii]
+        x0,y0 = int(self.x), int(self.y)
+                 
+        circapers = [CircularAperture((self.x-x0+dx,self.y-y0+dx), aprad ) for aprad in [1,1.5,2,2.5,3,3.5,4]]
+
+        self.apertures = circapers
+        
         return circapers
         
+
         
-    def _do_photometry(self, fname, bkg_estimate=5.23, sicid=1):
+    def _do_photometry(self, fname, bkg_estimate=5.23, dx=8):
+
+        x0,y0 = int(self.x), int(self.y)
+
+        x_range = x0-dx, x0+dx
+        y_range = y0-dx, y0+dx 
         
-        hdulist = fits.open(fname,mmap=False)
-        img=hdulist[0].data.T
-        err=hdulist[1].data.T
-        hdr=hdulist[0].header
+        circapers = self._make_apertures(self.aperture_radii, dx)
+
+        
+        fits=fitsio.FITS(fname)
+        
+        img = fits[0][x_range[0]:x_range[1], y_range[0]:y_range[1]]
+        err = fits[1][x_range[0]:x_range[1], y_range[0]:y_range[1]]
+        hdr = fits[0].read_header()
+                
+        #(img,err),hdr = fitsio.read(fname, rows=x_range, columns=y_range, header=True)
+                
+        #err = fitsio.read(fname, rows=x_range, columns=y_range,).T
+        
+        #hdulist = fits.open(fname,mmap=False)
+        #img=hdulist[0].data.T
+        #err=hdulist[1].data.T
+        #hdr=hdulist[0].header
         time = hdr['TSTART'] 
         
-        result = aperture_photometry(img - bkg_estimate, self.apertures, error=err, )
+        result = aperture_photometry(img.T - bkg_estimate, circapers, error=err)
+        
         result['bjd'] = time
         
-        del img
-        del err
-        del hdr
-        hdulist.close()
+        #del img
+        #del err
+        #del hdr
+        #hdulist.close()
         
-        del hdulist
+        #del hdulist
+        
+        fits.close()
         
         return result
     
     
-    def get_lightcurve(self,sicid=1):
+    def get_lightcurve(self):
         
-        all_results = vstack( [self._do_photometry(f,sicid) for f in tqdm(self.fnames)] )
+        all_results = vstack( [self._do_photometry(f) for f in self.fnames] )
         
         [all_results.rename_column('aperture_sum_'+str(i), 'apflux_r_'+str(ap.r)) for i,ap in enumerate(self.apertures)]
         [all_results.rename_column('aperture_sum_err_'+str(i), 'apflux_r_'+str(ap.r)+'_err') for i,ap in enumerate(self.apertures)]
         
         return vstack(all_results)
+        
+        
     
